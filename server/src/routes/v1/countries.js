dbe59626e2ce61607803d7538e7ed92eed318a2f
Abdurrahman Khurshed <khurshed@KHURSHED-PPC.local>
2021-09-28 00:13:09 +0200

    Add error handling for broken APIs

--- server/src/routes/v1/countries.js
+++ server/src/routes/v1/countries.js
@@ -11,11 +11,19 @@ router.get('/', async (req, res) => {
       `https://restcountries.com/v2/continent/${req.query.region}`
     );
 
-    res.json(restCountriesRes.data);
+    const countries = restCountriesRes.data;
+
+    if (!(Array.isArray(countries) && countries.length > 0)) {
+      return res.status(500).json({
+        message: `REST Countries APIs seem to be down. Please check https://restcountries.com/v2/continent/${req.query.region} for confirmation.`,
+      });
+    }
+
+    return res.json(countries);
   } catch (error) {
     logger.error(error.stack);
 
-    res
+    return res
       .status(500)
       .json({ message: 'An unexpected error has occurred.', statusCode: 500 });
   }
